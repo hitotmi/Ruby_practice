e42b54d0c77b8cf279c90039872aec785c490538
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-07-05 13:38:02 +0000

    [Add]RGB変換

---
+++ lib/rgb.rb
@@ -0,0 +1,19 @@
+def to_hex(r, g, b)
+  [r, g, b].sum('#') do |n|
+    n.to_s(16).rjust(2, '0')
+  end
+end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  [r, g, b].map do |s|
+    s.hex
+  end
+end
+
+# def to_ints(hex)
+#   # scanメソッドは、正規表現に一致する文字列を配列として返す
+#   hex.scan(/\w\w/).map(&:hex)
+# end
