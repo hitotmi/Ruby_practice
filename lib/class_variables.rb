c6a3aafd84b31d34f3018842dadccfd4fb4daa62
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-07-08 07:48:51 +0000

    [Add]クラス変数

---
+++ lib/class_variables.rb
@@ -0,0 +1,41 @@
+##クラス変数
+##@@nameのように変数名の最初に@を2つけると、そのクラスとそのサブクラスのインスタンス間で値を共有できる。
+class Product
+  @@name = 'Product'
+
+  def self.name
+    @@name
+  end
+
+  def initialize(name)
+    @@name = name
+  end
+
+  def name
+    @@name
+  end
+end
+
+class DVD < Product
+  @@name = 'DVD'
+
+  def self.name
+    @@name
+  end
+
+  def upcase_name
+    @@name.upcase
+  end
+end
+
+
+#DVDクラスを定義した時に@@nameがDVDに変更される
+puts Product.name
+puts DVD.name
+
+Product.new('movie')
+puts Product.name
+
+#Product.newの時に@@nameがmovuieに変更される
+puts Product.name
+puts DVD.name
