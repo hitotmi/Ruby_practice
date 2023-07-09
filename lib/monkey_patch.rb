b1915a3b34b93af4c326be72b09df0bc1d55f412
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-07-09 05:46:37 +0000

    [Add]モンキーパッチ

---
+++ lib/monkey_patch.rb
@@ -0,0 +1,23 @@
+class User
+
+  def initialize(name, email)
+    @name = name
+  end
+
+  def hello
+    "Hello, my name is #{@name}."
+  end
+end
+
+user = User.new("Alice", "alice@example.com")
+puts user.hello
+
+# Userクラスに対するモンキーパッチを適用して、helloメソッドを上書き
+class User
+  def hello
+    "#{@name}さん、こんにちは！！"
+  end
+end
+
+# 上書きしたhelloメソッドを呼び出して結果を表示
+puts user.hello
