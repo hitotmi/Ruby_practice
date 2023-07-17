41fe06bb1a2e630eb27e3b636586e4d159735f3e
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-07-17 04:36:59 +0000

    [Add]Procオブジェクトをブロックの代わりに渡す

--- lib/my_proc.rb
+++ lib/my_proc.rb
@@ -16,9 +16,27 @@ execute_proc(hello_proc)
 # Proc.newの作成と実行
 add_proc = Proc.new { |a, b| a + b }
 puts add_proc.call(10, 20)
+puts add_proc.yield(10, 20)
+puts add_proc.(10, 20)
+puts add_proc[10, 20]
+puts add_proc === [10, 20]
 puts add_proc.lambda?  #=>false
 
 # ランダの作成と実行
 add_lambda = ->(a, b) { a + b }
 puts add_lambda.call(10, 20)
 puts add_lambda.lambda?  #=>true
+
+
+def welcome(&block)
+  puts 'Hello'
+  text = block.call('Welcome here')
+  puts text
+  puts 'Goodbye'
+end
+
+# 引数のテキストを大文字に変換する Proc オブジェクトを作成
+upcase_proc = Proc.new { |text| text.upcase }
+# Proc オブジェクトをブロックとして welcome メソッドに渡す
+welcome(&upcase_proc)
+
